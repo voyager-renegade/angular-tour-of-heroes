49c06fa0c497e99f18c1e3d0f82825628ecb08e9
Freddy Duran <duran.freddy.g@gmail.com>
2017-09-03 11:57:48 -0400

    Chapter 4
    Signed-off-by: Freddy Duran <duran.freddy.g@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from "./hero";
 
-export class Hero {
-  id: number;
-  name: string;
-}
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -36,14 +33,8 @@ const HEROES: Hero[] = [
     </li>
   </ul>
 
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-  </div>
+  <hero-detail [hero]="selectedHero"></hero-detail>
+  
   `,
   styles: [`
   .selected {
